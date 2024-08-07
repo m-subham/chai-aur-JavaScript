1f5c2681fb8563ae620ad72e08101882422f2a2c
Subham Maharana <121706810+m-subham@users.noreply.github.com>
2024-08-07 21:25:25 +0530

    iife

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,16 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+(function chai () {
+    console.log("DB coonected");
+    
+})();
+
+(function aurcode(name){
+    console.log("DB connected too! " + name);
+    
+})("Subham");
+
+( () => {
+    console.log("DB coonected tooooooooooooooooo  ");
+    
+})();
